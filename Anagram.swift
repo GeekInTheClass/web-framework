fc84c28e1e912cfbf630288b6f5cb25210fb13b0
Sang Chul Lee <scott824@naver.com>
2017-03-30 16:32:20 +0900

    make more short code

--- Anagram.swift
+++ Anagram.swift
@@ -17,47 +17,12 @@ class Anagram {
     }
     
     func match(words: [String]) -> [String] {
-        /*
-        var result: [String] = []
-        let self_word = self.word
         
-        for word in words {
-            
-            if !isSameWord(word, self_word, insensitive: true)
-            && isSameLength(word, self_word) {
-                
-                var input_word_arr = Array(word.characters)
-                let self_word_arr = Array(self_word.characters)
-                
-                for char in self_word_arr {
-                    
-                    let low = Character(String(char).lowercased())
-                    let upper = Character(String(char).uppercased())
-                    
-                    if let index = input_word_arr.index(of: low) {
-                        input_word_arr.remove(at: index)
-                    }
-                    else if let index = input_word_arr.index(of: upper) {
-                        input_word_arr.remove(at: index)
-                    }
-                }
-                
-                if input_word_arr.isEmpty {
-                    result.append(word)
-                }
-            }
-        }
-        return result
-        */
-        
-        var result = words.filter({word in
+        return words.filter({word in
             return !isSameWord(word, self.word, insensitive: true)
                 && isSameLength(word, self.word)
+                && word.lowercased().characters.sorted(by: >) == self.word.lowercased().characters.sorted(by: >)
         })
-        result = result.filter({ word in
-            return word.lowercased().characters.sorted(by: >) == self.word.lowercased().characters.sorted(by: >)
-        })
-        return result
     }
 }
 
