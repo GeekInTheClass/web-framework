dac036d5ebffdf7762b5f971011353d2658a8c9c
KangByungWook <brillante91@naver.com>
2017-03-30 17:34:18 +0900

    Update Contents.swift

--- quiz.playground/Contents.swift
+++ quiz.playground/Contents.swift
@@ -1,6 +1,32 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+// 1. n부터 m까지 합 구하기
+var n = 10
+var m = 20
+let myRange: CountableRange = n..<m
+var result = 0
+for rangeElement in myRange {
+    result += rangeElement
+}
+result
+
+// 2. 2차원 배열에서 각 열의 평균을 1차원 배열로 변환
+var matrix = [[1.2,2.0,3.0],
+              [4.0,5.0,6.0],
+              [7.0,8.0,9.0]]
+var result_list = [0.0,0.0,0.0]
+// 일단 2차원 배열을 세로 방향으로 합친다.
+for element in matrix {
+    for (index, value) in element.enumerated() {
+        result_list[index] += value
+    }
+}
+// 합친 배열 요소 각각의 평균을 구한다.
+for (index,sum) in result_list.enumerated() {
+        result_list[index] = Double(sum) / Double(matrix.count)
+}
+result_list
 
 /* 3 */
 var wordArray = ["apple", "banana", "cat", "dog", "elephant", "fire", "god", "hippo"]
